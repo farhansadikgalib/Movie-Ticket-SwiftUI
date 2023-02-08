148df747d3c3fee2b104d6de68665e8aa0e2c682
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2023-02-08 15:12:18 +0600

    Initial Commit

---
+++ Movie Ticket/Movie_TicketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_TicketApp.swift
+//  Movie Ticket
+//
+//  Created by Farhan Sadik on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Movie_TicketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
